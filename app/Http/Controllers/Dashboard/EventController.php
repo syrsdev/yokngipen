394b8592a425be5ceb31e554c16397e33b52903a
Mohammad Afdanil Ferdi Kasim <danil04062000@gmail.com>
2025-12-25 22:47:51 +0700

    feat:detail event dan update event

--- app/Http/Controllers/Dashboard/EventController.php
+++ app/Http/Controllers/Dashboard/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\event_prices;
 use App\Models\events;
 use Illuminate\Http\Request;
 
@@ -31,12 +32,41 @@ class EventController extends Controller
         $event->prices()->create($credential2);
         return;
     }
-    // public function index()
-    // {
-    //     return view('dashboard.pages.dashboard');
-    // }
-    // public function index()
-    // {
-    //     return view('dashboard.pages.dashboard');
-    // }
+    public function detail($id)
+    {
+        $event = events::findOrFail($id);
+        $event_prices = event_prices::where('id_event',$id)->get();
+        return;
+    }
+    public function update_event(Request $request, $id) {
+
+        $credential = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'location' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'banner' => 'required',
+            'status' => 'required',
+        ]);
+        
+        $event = events::findOrFail($id);
+        $event->update($credential);
+        return;
+
+    }
+    public function update_event_price(Request $request, $id)
+    {
+        $credential2 = $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'quota' => 'required',
+            'payment_method' => 'required',
+            'account_number' => 'required',
+        ]);
+
+        $event_prices = event_prices::findOrFail($id);
+        $event_prices->update($credential2);
+        return;
+    }
 }
