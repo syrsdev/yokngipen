1f330ee18326632dfb69cde3d70627367316021d
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2026-01-14 02:55:57 +0700

    feat: data dashboard

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,13 +3,28 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $activeMenu = 'dashboard';
-        return view('dashboard.pages.dashboard', compact('activeMenu'));
+        $jumlahEvent = Event::count();
+        $eventBerlangsung = Event::where('status', 'published')->count();
+        $eventSelesai = 0;
+        $jumlahUser = 0;
+
+        $users = User::where('role', '!=', 'admin')->limit(5)->get();
+
+        if (Auth::user()->role == "organizer") {
+            $eventSelesai = Event::where('status', 'closed')->count();
+        } else {
+            $jumlahUser = User::where('role', '!=', 'admin')->count();
+        }
+        return view('dashboard.pages.dashboard', compact('activeMenu', 'jumlahEvent', 'eventBerlangsung', 'eventSelesai', 'jumlahUser', 'users'));
     }
 }
--- resources/views/dashboard/pages/dashboard.blade.php
+++ resources/views/dashboard/pages/dashboard.blade.php
@@ -18,7 +18,7 @@
                                     </div>
                                     <div style="width: fit-content" class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="font-semibold text-muted">Jumlah Event</h6>
-                                        <h6 class="mb-0 font-extrabold">112.000</h6>
+                                        <h6 class="mb-0 font-extrabold">{{ $jumlahEvent }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -35,7 +35,7 @@
                                     </div>
                                     <div style="width: fit-content" class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="font-semibold text-muted">Event Berlangsung</h6>
-                                        <h6 class="mb-0 font-extrabold">183.000</h6>
+                                        <h6 class="mb-0 font-extrabold">{{ $eventBerlangsung }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -53,7 +53,7 @@
                                         </div>
                                         <div style="width: fit-content" class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                             <h6 class="font-semibold text-muted">Event Selesai</h6>
-                                            <h6 class="mb-0 font-extrabold">112</h6>
+                                            <h6 class="mb-0 font-extrabold">{{ $eventSelesai }}</h6>
                                         </div>
                                     </div>
                                 </div>
@@ -72,14 +72,13 @@
                                         </div>
                                         <div style="width: fit-content" class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                             <h6 class="font-semibold text-muted">Jumlah User</h6>
-                                            <h6 class="mb-0 font-extrabold">112</h6>
+                                            <h6 class="mb-0 font-extrabold">{{ $jumlahUser }}</h6>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     @endif
-
                 </div>
                 <div class="row">
                     <div class="col-12">
@@ -93,117 +92,43 @@
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12 col-xl-4">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4>Profile Visit</h4>
-                            </div>
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-7">
-                                        <div class="d-flex align-items-center">
-                                            <svg class="bi text-primary" width="32" height="32" fill="blue"
-                                                style="width:10px">
-                                                <use xlink:href="assets/static/images/bootstrap-icons.svg#circle-fill" />
-                                            </svg>
-                                            <h5 class="mb-0 ms-3">Europe</h5>
-                                        </div>
-                                    </div>
-                                    <div class="col-5">
-                                        <h5 class="mb-0 text-end">862</h5>
-                                    </div>
-                                    <div class="col-12">
-                                        <div id="chart-europe"></div>
-                                    </div>
+                @if (auth()->user()->role == 'admin')
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4>Pengguna</h4>
                                 </div>
-                                <div class="row">
-                                    <div class="col-7">
-                                        <div class="d-flex align-items-center">
-                                            <svg class="bi text-success" width="32" height="32" fill="blue"
-                                                style="width:10px">
-                                                <use xlink:href="assets/static/images/bootstrap-icons.svg#circle-fill" />
-                                            </svg>
-                                            <h5 class="mb-0 ms-3">America</h5>
-                                        </div>
-                                    </div>
-                                    <div class="col-5">
-                                        <h5 class="mb-0 text-end">375</h5>
-                                    </div>
-                                    <div class="col-12">
-                                        <div id="chart-america"></div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-7">
-                                        <div class="d-flex align-items-center">
-                                            <svg class="bi text-danger" width="32" height="32" fill="blue"
-                                                style="width:10px">
-                                                <use xlink:href="assets/static/images/bootstrap-icons.svg#circle-fill" />
-                                            </svg>
-                                            <h5 class="mb-0 ms-3">Indonesia</h5>
-                                        </div>
-                                    </div>
-                                    <div class="col-5">
-                                        <h5 class="mb-0 text-end">1025</h5>
-                                    </div>
-                                    <div class="col-12">
-                                        <div id="chart-indonesia"></div>
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="table" id="table1">
+                                            <thead>
+                                                <tr>
+                                                    <th>Name</th>
+                                                    <th>Email</th>
+                                                    <th>Phone</th>
+                                                    <th>Role</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($users as $item)
+                                                    <tr>
+                                                        <td>{{ $item->name }}</td>
+                                                        <td>{{ $item->email }}</td>
+                                                        <td>{{ $item->phone }}</td>
+                                                        <td>{{ $item->role }}</td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                        <a href="{{ route('users.index') }}" class="mt-5 btn btn-primary w-100">Lihat
+                                            Semua</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-12 col-xl-8">
-                        <div class="card">
-                            <div class="card-header">
-                                <h4>Latest Comments</h4>
-                            </div>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table table-hover table-lg">
-                                        <thead>
-                                            <tr>
-                                                <th>Name</th>
-                                                <th>Comment</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td class="col-3">
-                                                    <div class="d-flex align-items-center">
-                                                        <div class="avatar avatar-md">
-                                                            <img src="{{ asset('/template/assets/compiled/jpg/5.jpg') }}">
-                                                        </div>
-                                                        <p class="mb-0 font-bold ms-3">Si Cantik</p>
-                                                    </div>
-                                                </td>
-                                                <td class="col-auto">
-                                                    <p class="mb-0 ">Congratulations on your graduation!</p>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td class="col-3">
-                                                    <div class="d-flex align-items-center">
-                                                        <div class="avatar avatar-md">
-                                                            <img src="{{ asset('/template/assets/compiled/jpg/2.jpg') }}">
-                                                        </div>
-                                                        <p class="mb-0 font-bold ms-3">Si Ganteng</p>
-                                                    </div>
-                                                </td>
-                                                <td class="col-auto">
-                                                    <p class="mb-0 ">Wow amazing design! Can you make another
-                                                        tutorial for
-                                                        this design?</p>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @endif
             </div>
             <div class="col-12 col-lg-3">
                 <div class="card">
@@ -219,14 +144,16 @@
                         </div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Visitors Profile</h4>
+                @if (auth()->user()->role == 'admin')
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Perbandingan Pengguna</h4>
+                        </div>
+                        <div class="card-body">
+                            <div id="chart-visitors-profile"></div>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <div id="chart-visitors-profile"></div>
-                    </div>
-                </div>
+                @endif
             </div>
         </section>
     </div>
