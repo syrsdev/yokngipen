e6d032a2afd8264d195c36395e8e85002029dbac
Mohammad Afdanil Ferdi Kasim <danil04062000@gmail.com>
2026-01-14 01:21:21 +0700

    feat: edit profile

---
+++ app/Http/Controllers/Dashboard/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('dashboard.profile', [
+            'user' => $request->user(),
+            'activeMenu' => 'profile'
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'name'  => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users,email,' . $request->user()->id,
+        ]);
+
+        $user = $request->user();
+        $user->update([
+            'name'  => $request->name,
+            'email' => $request->email,
+        ]);
+
+        return back()->with('success', 'Profile berhasil diperbarui');
+    }
+}
