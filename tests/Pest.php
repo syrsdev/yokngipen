43b691868babaf05115d6237afcc412810439019
Renanthera Areno Zulkarnain <jeyyy2313@gmail.com>
2026-06-20 18:39:00 +0700

    menambahkan selenium

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,8 +1,5 @@
 <?php
 
-pest()->extend(Tests\DuskTestCase::class)
-//  ->use(Illuminate\Foundation\Testing\DatabaseMigrations::class)
-    ->in('Browser');
 
 pest()->extend(Tests\DuskTestCase::class)
 //  ->use(Illuminate\Foundation\Testing\DatabaseMigrations::class)
